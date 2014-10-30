7fa1e29cd73138e0250a1e9c26af0227327e1c18
Eugenie Kang <kangeugenie@gmail.com>
2014-10-30 18:16:32 +0000

    homework

---
+++ KangEugenie/STUCK.rb
@@ -0,0 +1,32 @@
+# num_guesses = 0
+#
+# loop do
+#  print "What is your guess?"
+#  guess = gets.chomp.to_i
+#  num_guesses += 1
+# end  
+# end
+# 
+# Generate an integer from 0 to 10
+# puts (rand() ) * 10).to_i
+# puts rand(10)
+#
+
+def quiz
+	puts "I am thinking of a number between 1 to 10. Can you guess it?"
+end
+
+def print_quiz(quiz_title, quiz_guess)
+	if quiz_guess > 5
+		puts "Too low"
+	
+	elsif quiz_guess < 5
+		puts "Too high"
+	end
+# I AM STUCK ! S.O.S
+
+def num_guesses
+	puts 
+end
+
+
